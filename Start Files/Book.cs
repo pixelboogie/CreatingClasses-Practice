bcbc624a1ae7da591db001490c023fe529e7edec
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-06-10 09:57:21 -0700

    Add files via upload

---
+++ Start Files/Book.cs
@@ -0,0 +1,18 @@
+namespace Treehouse.MediaLibrary
+{
+  
+  public class Book{
+    
+    public string Title;
+    public string Author;
+    
+          public Book(string title, string author){
+          Title = title;
+          Author = author;
+      }
+    
+  }
+
+  
+}
+
---
+++ Start Files/Test.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Treehouse.MediaLibrary 
+{ 
+     class Test 
+    { 
+          public void writeIt(){
+                        Console.WriteLine("test");
+            }
+       
+
+             
+       }
+}
